47bb580e1a19c330b968a782e0fc49e550321f7e
BenEspinoza10 <50683392+BenEspinoza10@users.noreply.github.com>
2024-09-09 21:45:20 -0300

    Giros con giroscopio

--- Robot Negro/codigoPrincipal/giros.ino
+++ Robot Negro/codigoPrincipal/giros.ino
@@ -36,3 +36,37 @@ void girarDerecha () {
   Motor(50, 50);
   delay(250);
 }
+
+//Giro en 90 grados hacia la derecha con giroscopio
+void girarDerechaGiroscopio() {
+  //Primero frenar
+  Motor(0, 0);
+  delay(250);
+  mpu.calcGyroOffsets();  // Calibrar giroscopio
+  int angulo = leerGiroscopio();  
+  int anguloInicial;
+  while (angulo >= (anguloInicial- 90)) {
+    Motor(50, -50);
+    angulo = leerGiroscopio();
+  }
+  //avanzar para asegurarse de salir del giro
+  Motor(50, 50);
+  delay(250);
+}
+
+//Giro en 90 grados hacia la izquierda con giroscopio
+void girarIzquierdaGiroscopio() {
+  //Primero frenar
+  Motor(0, 0);
+  delay(250);
+  mpu.calcGyroOffsets();  // Calibrar giroscopio
+  int angulo = leerGiroscopio();  
+  int anguloInicial;
+  while (angulo <= (90 + anguloInicial)) {
+    Motor(-50, 50);
+    angulo = leerGiroscopio();
+  }
+  //avanzar para asegurarse de salir del giro
+  Motor(50, 50);
+  delay(250);
+}
